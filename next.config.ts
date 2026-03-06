e5f5f222e6842c545f335d5043b67668bd801743
Alexander Kiggen <alexander.kiggen@gmail.com>
2026-03-06 09:57:19 +0100

    Whitelist test.remarkt.nl en www.remarkt.nl

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,19 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://test.remarkt.nl https://www.remarkt.nl;",
+          }
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
